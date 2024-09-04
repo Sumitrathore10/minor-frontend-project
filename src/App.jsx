9184779b9bbb5bc167ae2b3e5236b59e78e5f23a
sumit <srathore132005@gmail.com>
2024-09-04 21:44:28 +0530

    make-home-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import React from 'react'
+import React from "react";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div className='h-screen w-full flex  box-border'>
-      <nav className='w-[15%] h-full bg-red-500 flex items-center flex-col  pt-5 px-3 '>
-        <a href="" className='text-white font-sans font-semibold border-gray-200 border-2 px-3 py-2 '>Add New Product</a>
-        <hr className='w-[80%] mt-4'/>
-        <h1 className= 'w-[80%] text-white font-sans font-semibold text-3xl mb-4'>category</h1>
-        <ul className='  w-[80%] '>
-          
-        <li className=' mb-3 flex  items-center gap-2 text-white'> <span className='w-[15px] h-[15px] rounded-full block bg-white'></span>cat 1</li>
-        </ul>
-      </nav>
-
-      <div className='w-[85%] h-full bg-slate-500'>
-        
-      </div>
+    <div className="h-screen w-full flex  box-border">
       
+      <Home />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Home = () => {
+  return (
+    <>
+    <Nav />
+      <div className="w-[85%] h-full flex  flex-wrap  bg-zinc-100 p-7 overflow-x-hidden overflow-y-auto ">
+        <div className="w-[20%] h-[40%] flex justify-center items-center m-2 pt-4 flex-col gap-2 shadow-lg rounded bg-white shadow-zinc-500 ">
+          <div
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
+            }}
+            className="w-[80%] h-[80%]   bg-contain bg-no-repeat bg-center hover:scale-110 "
+          ></div>
+
+          <h1 className="font-sans text-red-500 hover:text-blue-500 cursor-pointer font-semibold">
+            School Bag
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
